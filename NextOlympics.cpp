962de0b95d1a9adb527c25f1640598a3e6d9f5f5
Brian Choi <humblesticker@Brians-MacBook-Pro.local>
2020-08-29 22:05:29 -0700

    day calculation without using tm and time_t

--- NextOlympics.cpp
+++ NextOlympics.cpp
@@ -1,27 +1,46 @@
 /*
 2021.07.23
 today: calculate days remaining  
+
+month array from 1 to 12
+year, month, day
+
+while date is not same
+	add day 
+	if day > max for the month day = 1, month++
+	if month > 12 month = 1 and year++
+	count++
+
+return count
+	
 */
-#include <ctime>
-#include <string>
 #include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
 
 class NextOlympics {
-	const string Olympic = "2021.07.23";
+	vector<int> Months{ 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	int Year = 2021, Month = 7, Day = 23;
 
 public:
+	// todo: you can advance day up to max to optimize
 	int countDays(string today) {
-		struct tm now = { 0 }, start = { 0 };
-   		strptime(today.c_str(), "%Y.%m.%d", &now);
-   		strptime(Olympic.c_str(), "%Y.%m.%d", &start); 
-		   
-        return difftime(mktime(&start), mktime(&now)) / (24 * 60 * 60);
+		int y = stoi(today.substr(0, 4)), m = stoi(today.substr(5, 2)), d = stoi(today.substr(8, 2)), count = 0;
+		while(!(y == Year && m == Month && d == Day)) {
+			count++; d++; 
+			if(d > Months[m]) { 
+				d = 1; 
+				m++; if(m > 12) { m = 1; y++; }
+			}
+		}
+		return count;
 	}
 };
 
 int main() {
+	string s; cin >> s;
 	NextOlympics no;
-	cout << no.countDays("2020.07.24") << endl;
+	cout << no.countDays(s) << endl;
 	return 0;
 }
